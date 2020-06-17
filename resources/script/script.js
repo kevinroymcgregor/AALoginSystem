6b9ce88ff37f882758a23a8033a5f788176beaa9
Kevin McGregor <kevin.mcgregor85204@gmail.com>
2020-06-16 21:35:12 -0700

    Initial site setup with console log of text value

---
+++ resources/script/script.js
@@ -0,0 +1,9 @@
+
+
+const subScript = () => {
+    const empID = document.getElementById("employeeID").value;
+    console.log(empID);
+}
+
+document.getElementById("subButton").addEventListener("click", subScript);
+
