31bd33fd47fc5438480ce80f45a3bf6e74e4655e
Kevin McGregor <kevin.mcgregor85204@gmail.com>
2020-06-18 21:58:00 -0700

    Added jQuery and post call to ADP test server

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,9 +1,63 @@
+$(document).ready(
+    function () {
+        const subScript = () => {
+            const empID = document.getElementById("employeeID").value;
+            $.post("https://test-api.adp.com/events/time/v1/clock.punch", {
+                "events": [
+                    {
+                        "serviceCategoryCode": {
+                            "codeValue": "core"
+                        },
+                        "eventNameCode": {
+                            "codeValue": "clockOffline.punch"
+                        },
+                        "originator": {
+                            "associateOID": empID
+                        },
+                        "actor": {
+                            "associateOID": empID
+                        },
+                        "data": {
+                            "eventContext": {
+                                "associateOID": empID
+                            },
+                            "transform": {
+                                "clockEntry": {
+                                    "deviceDateTime": "2020-06-18T00:00:00-04:00",
+                                    "entryDateTime": "2020-06-18T00:00:00-04:00",
+                                    "actionCode": {
+                                        "codeValue": "transfer"
+                                    },
+                                    "validGeoLocationID": "12",
+                                    "laborAllocations": [
+                                        {
+                                            "allocationCode": {
+                                                "codeValue": "205"
+                                            },
+                                            "allocationTypeCode": {
+                                                "codeValue": "project"
+                                            }
+                                        },
+                                        {
+                                            "allocationCode": {
+                                                "codeValue": "001"
+                                            },
+                                            "allocationTypeCode": {
+                                                "codeValue": "location"
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                ]
+            },
+                function (data, status) {
+                    console.log("Status: " + status)
+                })
+        }
 
-
-const subScript = () => {
-    const empID = document.getElementById("employeeID").value;
-    console.log(empID);
-}
-
-document.getElementById("subButton").addEventListener("click", subScript);
-
+        document.getElementById("subButton").addEventListener("click", subScript);
+    }
+)
